35605838b8561ffced4d60bc47ebdbda9cb3072d
draganasavanovic <dragana.savanovic@wayseven.com>
2019-10-29 12:57:23 +0100

    created slick slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'slick-slider';
+  title = "slick-slider";
+
+  slides = [
+    { img: "../assets/miki/miki1.jpg" },
+    { img: "../assets/miki/miki2.jpg" },
+    { img: "../assets/miki/miki3.jpg" },
+    { img: "../assets/miki/miki4.jpg" },
+    { img: "../assets/miki/miki5.jpg" },
+    { img: "../assets/miki/miki6.jpg" },
+    { img: "../assets/miki/miki7.jpg" },
+    { img: "../assets/miki/miki8.jpg" },
+    { img: "../assets/miki/miki9.jpg" },
+    { img: "../assets/miki/miki10.jpg" }
+  ];
+
+  slideConfig = {
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: "<div class='nav-btn next-slide'></div>",
+    prevArrow: "<div class='nav-btn prev-slide'></div>",
+    dots: true,
+    infinite: false
+  };
+
+  addSlide() {
+    this.slides.push({ img: "../assets/miki/miki11.jpg" });
+  }
+
+  removeSlide() {
+    console.log("remove");
+    this.slides.length = this.slides.length - 1;
+  }
+
+  slickInit(e) {
+    console.log("slick initialized");
+  }
+
+  breakpoint(e) {
+    console.log("breakpoint");
+  }
+
+  afterChange(e) {
+    console.log("afterChange");
+  }
+
+  beforeChange(e) {
+    console.log("beforeChange");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { SlickCarouselModule } from "ngx-slick-carousel";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
+import { ModalModule } from "ngx-bootstrap/modal";
+
+// import { OwlModule } from 'ngx-owl-carousel';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlickCarouselModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
